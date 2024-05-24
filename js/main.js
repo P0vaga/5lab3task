ac066e80edcab2cc193c20069353f01db0cae80a
Sasha Khapko <sxapko2211@gmail.com>
2024-05-24 11:57:04 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Константи
+document.getElementById('e').textContent = Math.E;
+        document.getElementById('pi').textContent = Math.PI;
+        document.getElementById('log10e').textContent = Math.LOG10E;
+        document.getElementById('log2e').textContent = Math.LOG2E;
+        document.getElementById('ln10').textContent = Math.LN10;
+        document.getElementById('ln2').textContent = Math.LN2;
+        document.getElementById('sqrt2').textContent = Math.SQRT2;
+        document.getElementById('sqrt1_2').textContent = Math.SQRT1_2;
+
+        // Функції
+        document.getElementById('max').textContent = Math.max(4, 2);
+        document.getElementById('min').textContent = Math.min(4, 2);
+        document.getElementById('pow').textContent = Math.pow(4, 2);
+        document.getElementById('sqrt').textContent = Math.sqrt(4);
+        document.getElementById('sqrtNeg').textContent = Math.sqrt(-9);
+
+        // Генератор випадкових чисел
+        document.getElementById('random').textContent = Math.random();
